e493d1cbac67bd904f04791700c208eabfd302df
Bilelxdz <35871833+Bilelxdz@users.noreply.github.com>
2020-12-21 08:39:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import Todo from './Todo'
 import Get from './GetData'
 
 function App() {
-  /*const [todos,setTodos]= useState([])
-  console.log(todos)*/
+  const [todos,setTodos]= useState([])
+  console.log(todos)
   return (
     <div className="App">
       <Get />
-      {/*<Add test={setTodos}/>}
-      {/*todos.map((elem) => {
+      {<Add test={setTodos}/>}
+      {todos.map((elem) => {
        return <Todo change={setTodos} data={elem} key={todos.id} />
-      })*/}
+      })}
     </div>
   );
 }
